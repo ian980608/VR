d34693f42dd0f4532fc2be5675f30dec9e11f8db
ian980608 <a0986936280@gmail.com>
2024-11-28 11:45:00 +0800

    1

---
+++ department store2/data.js
@@ -0,0 +1,259 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "九樓手扶梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.06530172097278353,
+        "pitch": 0.033075393102123485,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2190741792035027,
+          "pitch": 0.35579963388295965,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -3.0911770829722087,
+          "pitch": 0.2204570301170392,
+          "rotation": 0,
+          "target": "1-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-9",
+      "name": "9美食街",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8015329875245136,
+          "pitch": 0.18761813597382115,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.31887252815593214,
+          "pitch": 0.11536108921170118,
+          "rotation": 0,
+          "target": "2-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-9",
+      "name": "蘭屋9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.599636491135259,
+        "pitch": 0.11258848623696238,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8194298396051298,
+          "pitch": 0.27847926759865516,
+          "rotation": 0.7853981633974483,
+          "target": "1-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "十樓手扶梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.293714418145278,
+        "pitch": 0.26769398649393494,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8275795187151331,
+          "pitch": 0.4348483000074417,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.9813422666305094,
+          "pitch": 0.2014698890922979,
+          "rotation": 5.497787143782138,
+          "target": "4-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-10",
+      "name": "10售票口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.6569987761784013,
+        "pitch": 0.2546074743707969,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0792066244436596,
+          "pitch": 0.5027033939781571,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.18277619006948242,
+          "pitch": 0.474861112817873,
+          "rotation": 0,
+          "target": "5-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-10",
+      "name": "劇場入口10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.490691172214765,
+        "pitch": -0.07123930821995828,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.107620357096531,
+          "pitch": 0.39203983336947346,
+          "rotation": 0,
+          "target": "4-10"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "大園百9~10樓",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
