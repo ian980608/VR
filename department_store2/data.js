93e6b0d6f4a5bbbac4bfada53a06fe7e8bfc2023
ian980608 <a0986936280@gmail.com>
2024-12-12 11:38:40 +0800

    3

--- department_store2/data.js
+++ department_store2/data.js
@@ -42,7 +42,20 @@ var APP_DATA = {
           "target": "1-9"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+	          {
+          "yaw": -1.0422456256708763,
+          "pitch": 0.12182309608157738,
+          "title": "新葡苑",
+          "text": "<span style=\"color: rgb(52, 52, 52); font-family: Arial, 微軟正黑體, &quot;Microsoft JhengHei&quot;, &quot;Heiti TC&quot;, 黑體, sans-serif, serif; font-size: 16px; font-weight: 700; background-color: rgb(255, 255, 255);\">板橋新葡苑位於大遠百9樓，以港式料理、飲茶為主軸，融入創新手法、與健康概念呈現，為喜愛佳餚的饕客們提供一個私人聚會、公司聚餐、婚宴廣場的新選擇。</span>"
+        },
+        {
+          "yaw": -2.808651309701082,
+          "pitch": 0.0169675916952734,
+          "title": "勝勢豬排",
+          "text": "<span style=\"color: rgb(52, 52, 52); font-family: Arial, 微軟正黑體, &quot;Microsoft JhengHei&quot;, &quot;Heiti TC&quot;, 黑體, sans-serif, serif; font-size: 16px; font-weight: 700; background-color: rgb(255, 255, 255);\">「伊勢路-勝勢日式豬排」由慕里諾餐飲自日本引進，以「極黑・黑豚里肌豬排套餐」創造話題，並以高達四成”炸海鮮料理”為品牌特色。承襲日本講究「不時不食」的飲食文化，特選當令海陸食材入菜，聘請日本熟成豬排達人「星合和」為顧問指導，悉心講究每個環節，讓賓客享受日本究極炸豬排體驗。</span>"
+        }
+	  ]
     },
     {
       "id": "1-9",
@@ -86,7 +99,20 @@ var APP_DATA = {
           "target": "2-9"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+	          {
+          "yaw": 1.5090249683817598,
+          "pitch": 0.169303869026594,
+          "title": "非常泰",
+          "text": "<span style=\"color: rgb(52, 52, 52); font-family: Arial, 微軟正黑體, &quot;Microsoft JhengHei&quot;, &quot;Heiti TC&quot;, 黑體, sans-serif, serif; font-size: 16px; font-weight: 700; background-color: rgb(255, 255, 255);\">非常泰概念餐坊－泰式美食享樂地。結合美食與時潮的時尚泰式餐廳指標。以色彩縈繞的氛圍、風格多元的樂音，辛辣美味的菜餚，搭配異國啤酒，提供一個輕鬆享樂的地方，滿足您對美味的期待、暢享歡聚時光！</span>"
+        },
+        {
+          "yaw": -1.312847777873106,
+          "pitch": 0.16714571208046003,
+          "title": "古拉爵",
+          "text": "<span style=\"color: rgb(52, 52, 52); font-family: Arial, 微軟正黑體, &quot;Microsoft JhengHei&quot;, &quot;Heiti TC&quot;, 黑體, sans-serif, serif; font-size: 16px; font-weight: 700; background-color: rgb(255, 255, 255);\">提供顧客輕鬆歡聚,享用多樣化義大利料理，義大利麵，以多樣化的口味來滿足顧客的期待，自家配方現場製作的道地拿坡里比薩，外酥內Q的餅皮,搭配各式各樣豐富的進口高級食材，讓顧客能如臨義大利當地的體驗。</span>"
+        }
+	  ]
     },
     {
       "id": "2-9",
@@ -208,7 +234,14 @@ var APP_DATA = {
           "target": "5-10"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+	          {
+          "yaw": 1.8963471151040512,
+          "pitch": 0.2694321253752143,
+          "title": "售票口",
+          "text": "Text"
+        }
+	  ]
     },
     {
       "id": "5-10",
@@ -246,7 +279,20 @@ var APP_DATA = {
           "target": "4-10"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+	          {
+          "yaw": 1.7939131508783657,
+          "pitch": 0.32786420376841363,
+          "title": "卡拉OK機",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4791488953527683,
+          "pitch": 0.18200070837588989,
+          "title": "入口",
+          "text": "Text"
+        }
+	  ]
     }
   ],
   "name": "大園百9~10樓",
